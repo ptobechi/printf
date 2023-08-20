a5bf08a16e6f88b4af49c796aff20a20d1e13df8
ptobechi <ptobechii@gmail.com>
2023-08-20 13:55:29 -0700

    print innteger

--- 1-printf.c
+++ 1-printf.c
@@ -8,6 +8,34 @@
  */
 void handle_int(va_list list, int *count)
 {
-	_putchar(va_arg(list, int));
-	(*count)++;
+	int digit = va_arg(list, int);
+	int digitCount = 0;
+
+	if (digit == 0)
+		digitCount = 1;
+	else
+	{
+		while (digit != 0)
+		{
+			digit /= 10;
+			digitCount++;
+		}
+	}
+
+	if (digit < 0)
+	{
+		_putchar('-');
+		(*count)++;
+		digit = -digit;
+	}
+
+	for (i = 0; i < digitCount; i++)
+	{
+		int dividor = 1;
+		for (j = 1; j < digitCount- i; j++)
+			divisor *= 10;
+		int digit = (num / divisor) % 10;
+		_putchar('0' + digit);
+		(*count)++;
+	}
 }
