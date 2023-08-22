b16424f42b9092fc3d3520315aaa1995340b668f
ptobechi <ptobechii@gmail.com>
2023-08-22 11:00:02 -0700

    printf

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-#define BUFFER_SIZE 1024
 
 /**
  * struct _search - function and specifier structure
@@ -17,8 +16,6 @@ typedef struct _search
 	const char str;
 	int (*f)();
 } _search;
-void flush_buffer();
-void handle_buffer(char c);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int handle_string(va_list list);
